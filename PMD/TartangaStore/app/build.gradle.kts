5256a0f89dbc17cf2ebef7f6f951f52b0fd321d7
AKGM-17 <ekaintorresmiranda@gmail.com>
2026-01-21 14:24:04 +0100

    Android es tu dio(s)

--- PMD/TartangaStore/app/build.gradle.kts
+++ PMD/TartangaStore/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.tartangastore"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tartangastore"
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
