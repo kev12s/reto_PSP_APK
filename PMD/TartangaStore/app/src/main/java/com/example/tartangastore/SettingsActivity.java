7c67f8d9533b33ebb6fed21ac925ad07dfc82c13
AKGM-17 <ekaintorresmiranda@gmail.com>
2026-01-28 14:14:09 +0100

    ahora si, a mergear

--- PMD/TartangaStore/app/src/main/java/com/example/tartangastore/SettingsActivity.java
+++ PMD/TartangaStore/app/src/main/java/com/example/tartangastore/SettingsActivity.java
@@ -1,2 +1,15 @@
-package com.example.tartangastore;public class SettingsActivity {
+package com.example.tartangastore;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class SettingsActivity  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        private Button btnCrear;
+
+    }
 }
