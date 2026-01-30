6fe7a040028b7a8524f91a5c30692c5a4976c65b
AKGM-17 <ekaintorresmiranda@gmail.com>
2026-01-30 14:25:54 +0100

    header bonito

--- PMD/TartangaStore/app/src/main/java/com/example/tartangastore/SettingsActivity.java
+++ PMD/TartangaStore/app/src/main/java/com/example/tartangastore/SettingsActivity.java
@@ -5,11 +5,29 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.filament.View;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
 public abstract class SettingsActivity  extends AppCompatActivity {
+
+    private Button btnHome;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        private Button btnCrear;
+        btnHome=findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            private void onClick(View v) {
+                // Abrir segunda Activity
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
